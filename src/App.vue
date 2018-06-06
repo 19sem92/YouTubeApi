aacfe9ce766752c4e8a8ef1be072034ea4850309
Sergey Chirva <19sem92@gmail.com>
2018-06-06 21:00:45 +0300

    VideoList component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        Hi there!
+        <SearchBar @termChange="onTermChange"/>
+        <VideoList/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import axios from 'axios';
+    import SearchBar from './components/SearchBar';
+    import VideoList from './components/VideoList';
+    const API_KEY = 'AIzaSyBdsUPqT6cwQ-E2h7lWtPDy-m8vmjdHMTU';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            SearchBar,
+            VideoList
+        },
+        methods: {
+            onTermChange(searchWord){
+
+                axios.get('https://www.googleapis.com/youtube/v3/search', {
+                    params:{
+                        key: API_KEY,
+                        type: 'video',
+                        part: 'snippet',
+                        q: searchWord
+                    }
+
+                })
+                    .then(res => console.log(res))
+            }
+        }
+
+    }
+
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,19 @@
+<template>
+ <div>
+     <input @input.lazy="onInput" type="text">
+ </div>
+</template>
+
+<script>
+    export default {
+        name: 'SearchBar',
+        methods: {
+            onInput(e){
+                this.$emit('termChange', e.target.value)
+            }
+        }
+    }
+
+</script>
+
+<style></style>
